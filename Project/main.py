3b0eb66501adebb76bdab89c8fca9ed457a76793
samuel.dansereau <samuel.dansereau@mymail.champlain.edu>
2023-11-28 13:26:38 -0500

    I got streamlit working

--- Project/main.py
+++ Project/main.py
@@ -1,4 +1,15 @@
-import pandas
-pip install streamlit
+import pandas as pd
+import streamlit as sl
 
-read_csv('vgsales.csv')
+df = pd.read_csv('vgsales.csv')
+
+test = "TEST"
+
+test
+
+#df = pd.DataFrame({'first column': [1,2,3,4],
+   #                'second column': [5,3,7,8]
+    #               })
+
+df
+
