bd45d97ac1eaa78aca8db9c7cfecee0cf6262714
samuel.dansereau <samuel.dansereau@mymail.champlain.edu>
2023-12-01 13:22:33 -0500

    Added a second page

--- Project/pages/main.py
+++ Project/pages/main.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import streamlit as sl
 
-file = 'vgsales.csv'
-#game = pd.read_csv(file)
+sl.markdown("# Home Page")
+sl.sidebar.markdown("# Home Page")
+
+file = '../CSV Files/vgsales.csv'
+game = pd.read_csv(file)
 
 test = "TEST"
 
@@ -13,4 +16,6 @@ df = pd.DataFrame({'first column': [1,2,3,4],
                    })
 
 sl.line_chart(df)
+
+
 
---
+++ Project/pages/pagetwo.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as sl
+
+sl.markdown("# Second Page")
+sl.sidebar.markdown("# Second Page")
+
+text = "Welcome to Page 2"
+
+text
